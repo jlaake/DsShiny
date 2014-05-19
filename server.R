e38d3551346c14e77baf799fc3a52c35c719377c
Jeff Laake <jefflaake@gmail.com>
2014-05-19 05:58:52 -0700

    additions for fitting model

--- server.R
+++ server.R
@@ -1,46 +1,104 @@
-
-# This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
 library(mrds)
-data(book.tee.data)
-tees=book.tee.data$book.tee.dataframe
-result=ddf(dsmodel = ~mcds(key = "hn", formula = ~1), data = tees[tees$observer ==1, ], 
-           method = "ds", meta.data = list(width = 4))
 
 chitable <- function(observed, expected) {
-  x = rbind(observed, expected, (observed - expected)^2/expected)
-  x = cbind(x, apply(x, 1, sum))
-  colnames(x)[ncol(x)]="Total"
-  x=rbind(bin=colnames(x),x)
-  rownames(x) = c("Bin","Observed", "Expected", "Chisquare")
-  return(x)
+	x = rbind(observed, expected, (observed - expected)^2/expected)
+	x = cbind(x, apply(x, 1, sum))
+	colnames(x)[ncol(x)]="Total"
+	x=rbind(bin=colnames(x),x)
+	rownames(x) = c("Bin","Observed", "Expected", "Chisquare")
+	return(x)
 }
 
-shinyServer(function(input, output) {
-    bins = reactive(seq(0, 4, length.out = input$bins + 1))
-    gof = reactive(ddf.gof(result,qq=FALSE,breaks=bins()))
-    
-    output$distPlot <- renderPlot({
-      # plot the data with those bins
-      plot(result,breaks=bins())
-     })
-   
-    output$gof <- renderDataTable(
-      {
-        # compute gof table
-        t(chitable(gof()$chisquare$chi1$observed, gof()$chisquare$chi1$expected))
-      })
-    output$P <- renderText( 
-      {
-        if(!is.na(gof()$chisquare$chi1$p)){
-        paste("\nP =",format(gof()$chisquare$chi1$p,digits=5),
-                " with ",gof()$chisquare$chi1$df," degrees of freedom\n",sep="")
-        }else{
-         "\nNo degrees of freedom for test\n"
-      }})
-})
+shinyServer(function(input, output,session) {
+# Load data from file; update width with max distance
+			data <- reactive({
+						if (is.null(input$datafile)) {
+							# User has not uploaded a file yet
+							return(NULL)
+						}
+						else
+						{
+							obs<-read.delim(input$datafile$datapath)
+							if(input$width==0)
+								width<-max(obs$distance)
+							else
+								width<-input$width
+						}
+						updateNumericInput(session,"width",value=width)
+						return(list(obs=obs,width=width))
+					})
+# Render to data table
+			output$datatable <- renderDataTable(data()$obs)
+# Once go pressed, fit model
+			results <- reactive({
+						if (input$goButton == 0)
+							return(NULL)
+						else
+						{
+							input$goButton
+							isolate({
+										if(is.null(data()))return(NULL)
+										df=data()$obs
+										key<-switch(input$keyfct,
+												"Half-normal"="hn",
+												"Hazard rate"="hr",
+												"Uniform"="unif",
+												"Gamma"="gamma")
+										adj<-switch(input$adjfct,
+												"Cosine"="cos",
+												"Simple Polynomial"="poly",
+												"Hermite Polynomial"="herm",
+												"None"=NULL)
+										if(is.null(adj))
+										{
+											eval(parse(text=paste('ddf(dsmodel = ~mcds(key = "',key,'",formula = ~size), data = df[df$observer==1, ],method = "ds", meta.data = list(width = data()$width))',sep="")))  
+										} else
+										{
+											eval(parse(text=paste('ddf(dsmodel = ~mcds(key = "',key,'",adj.series="',adj,'",formula = ~size), data = df[df$observer==1, ],method = "ds", meta.data = list(width = data()$width))',sep="")))
+										}})
+						}
+					})
+# Reactive to number of bins, compute gof
+			gof <- reactive({
+						if(is.null(data()))return(NULL)
+						bins<-seq(0, data()$width, length.out = input$bins + 1)
+						gof <- ddf.gof(results(),qq=FALSE,breaks=bins)
+						list(bins=bins,gof=gof)
+					})
+# Reactive to number of bins, produce and output plot
+			output$distPlot <- renderPlot({
+						if (input$goButton == 0) return()
+						else
+						{
+							if(is.null(data()))return(NULL)
+							plot(results(),breaks=gof()$bins)
+						}
+					})
+# Once go button pushed output gof chi-square table
+			output$gof <- renderDataTable(
+					{
+						if (input$goButton == 0)
+							return()
+						else
+						{
+							if(is.null(data()))return(NULL)
+							t(chitable(gof()$gof$chisquare$chi1$observed, gof()$gof$chisquare$chi1$expected))                
+						}
+					})
+# Once go button pushed output gof chi-square P-value
+			output$P <- renderText( 
+					{
+						if (input$goButton == 0)
+							return()
+						else
+						{
+							if(is.null(data()))return(NULL)
+							if(!is.na(gof()$gof$chisquare$chi1$p)){
+								paste("\nP =",format(gof()$gof$chisquare$chi1$p,digits=5),
+										" with ",gof()$gof$chisquare$chi1$df," degrees of freedom\n",sep="")
+							}else{
+								"\nNo degrees of freedom for test\n"
+							}
+						}})
+		})
--- ui.R
+++ ui.R
@@ -8,25 +8,34 @@
 library(shiny)
 
 shinyUI(fluidPage(
-
-  # Application title
-  titlePanel("Single Observer Distance Sampling"),
-
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 4),
-      htmlOutput("P")
-    ),
-
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot"),
-      dataTableOutput("gof")
-    )
-  )
-))
+				
+				# Application title
+				titlePanel("Single Observer Distance Sampling"),
+				
+				# Sidebar with a slider input for number of bins
+				sidebarLayout(
+						sidebarPanel(
+								fileInput("datafile","Data file"),
+								numericInput("width","Width",value=0,min=0),
+								selectInput("keyfct","Key Function",choices=c("Unif","Half-normal","Hazard rate","Gamma"),selected="Half-normal"),
+								selectInput("adjfct","Adjustment Function",choices=c("None","Cosine","Simple Polynomial","Hermite polynomial"),selected="None"),
+								actionButton("goButton", "Fit model"),
+								hr(),
+								sliderInput("bins",
+										"Number of bins:",
+										min = 1,
+										max = 50,
+										value = 4),
+								htmlOutput("P")
+						),
+						
+						# Show a plot of the generated distribution
+						mainPanel(		
+								tabsetPanel(type = "tabs", 
+										tabPanel("Data",dataTableOutput("datatable")),
+										tabPanel("Plot", plotOutput("distPlot")),  
+										tabPanel("GOF", dataTableOutput("gof"))
+								)
+						)
+				)
+		))
